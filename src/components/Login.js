d083cf5a5e2f3cbe8527465e3490198167e53cb4
SeymaDemir <seymademir35@gmail.com>
2023-04-28 21:27:12 +0300

    added responsive design to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,30 +4,30 @@ import logo from "../images/sofa.png";
 
 function Login() {
   return (
-    <div className="text-dark1 font-bakbak bg-light2 h-screen items-center flex flex-col justify-center gap-8">
+    <div className="text-dark1 font-bakbak bg-light2 h-screen items-center flex flex-col justify-center lg:gap-8 gap-4">
       <div className="flex flex-col items-center">
-        <img src={logo} alt="logo" className="w-[10em] mb-[-2em]" />
-        <div className="text-[2em] text-center  text-[#B939B7]">Hoomie</div>
+        <img src={logo} alt="logo" className="lg:w-[10em] w-[5em] lg:mb-[-2em]" />
+        <div className="lg:text-[2em] text-lg text-center text-[#B939B7]">Hoomie</div>
       </div>
-      <div className=" font-montserrat flex flex-col items-center  gap-2">
-        <div className="text-5xl text-dark1 font-bold">
+      <div className=" font-montserrat flex flex-col items-center gap-2">
+        <div className="lg:text-5xl text-lg text-dark1 font-bold">
           Log in to your account
         </div>
-        <div className="text-lg cursor-pointer underline text-blue-600">
+        <div className="lg:text-lg text-sm cursor-pointer underline text-blue-600">
           <Link to="/register"> Don't have an account? </Link>
         </div>
       </div>
-      <div className="flex flex-col gap-6 w-[30em] font-montserrat font-bold">
+      <div className="flex flex-col lg:gap-6 gap-4 lg:w-[30em] w-[15em] font-montserrat lg:font-bold">
         <input
           placeholder="Username or Email"
-          className="h-[3em] placeholder:tracking-wider placeholder:pl-4"
+          className="lg:h-[3em] h-[2em] placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
         />
         <input
           placeholder="Password"
-          className="h-[3em] placeholder:tracking-wider placeholder:pl-4"
+          className="lg:h-[3em] h-[2em] placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
         />
         <div className="self-center">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 rounded text-xl tracking-widest ">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold lg:py-3 py-2 lg:px-20 px-8 rounded lg:text-xl text-sm tracking-widest ">
             Login
           </button>
         </div>
