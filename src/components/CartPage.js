ad4b12f6b889595af9fe3246a617cdee6edc5447
SeymaDemir <seymademir35@gmail.com>
2023-05-18 22:49:08 +0300

    .

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function CartPage() {
   return (
-    <div className='flex lg:flex-row flex-col items-center h-screen justify-center justify-evenly'>
-        <div>orders</div>
-        <div>order summary</div>
+    <div className="flex lg:flex-row flex-col items-center h-screen justify-center font-montserrat">
+      <div className="flex lg:flex-row flex-col border rounded-xl p-4 lg:mt-10 sm:p-6 lg:p-8 dark:border-gray-700 shadow-lg bg-white min-w-[90vw]">
+        <div className="basis-1/2">
+          <div className="text-3xl flex lg:flex-row flex-col justify-between border-b-2 pb-10 font-bold">
+            <div>Shopping Bag</div>
+            <div>2 Items</div>
+          </div>
+          <div>products</div>
+        </div>
+        <div className="basis-1/2 items-center flex flex-col ">
+          <div className="text-3xl border-b-2 pb-10 pl-40 pr-40">Order Summary</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CartPage
+export default CartPage;
