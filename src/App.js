d49a3c57f8e28b1f8d9ad4889043ae232c44a808
SeymaDemir <seymademir35@gmail.com>
2023-05-20 10:51:36 +0300

    cleaned cart page extras

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from "./components/About"
-import CardPage from "./components/CardPage"
 // import Footer from './components/Footer';
 // import HomePage from './components/HomePage';
 import LandingPage from './components/LandingPage';
@@ -24,7 +23,6 @@ function App() {
     <Routes>
       <Route exact path="/" element={<LandingPage/>}/>
       <Route exact path="/about" element={<About/>}/>
-      <Route exact path="/cardpage" element={<CardPage/>}/>
       <Route exact path="/product" element={<Product/>}/>
       <Route exact path="/products" element={<Products/>}/>
       <Route exact path="/profile" element={<Profile/>}/>
