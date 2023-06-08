a94df8d5b663f494d1ffd16c135dc73c60c683f3
SeymaDemir <seymademir35@gmail.com>
2023-06-08 12:27:22 +0300

    completed delivery part

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
@@ -13,6 +13,9 @@ const validationSchema = Yup.object().shape({
 });
 
 function OrderPage() {
+  //delivery ile payment method butonları arası gecisi tutar
+  const [isToggle, setToggle] = useState(true)
+
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -30,136 +33,142 @@ function OrderPage() {
     },
   });
 
+  const deliveryPart =  <div className=" min-w-[65vw] h-auto p-10 bg-white rounded-[1em] shadow-2xl">
+  <form
+    onSubmit={formik.handleSubmit}
+    className="flex flex-col min-w-[60vw] place-self-center w-[30vw] gap-5"
+  >
+    <div className="flex lg:flex-row flex-col lg:justify-between gap-5 lg:gap-0 ">
+      <div>
+        <input
+          className="border-2 lg:h-[3em] h-[2em] lg:min-w-[30em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+          type="text"
+          name="name"
+          placeholder="First name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.name && formik.errors.name && (
+          <p className="text-red-500">{formik.errors.name}</p>
+        )}
+      </div>
+      <div>
+        <input
+          className="border-2 lg:h-[3em] h-[2em] lg:min-w-[30em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+          type="text"
+          name="lastname"
+          placeholder="Last name"
+          value={formik.values.lastname}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.lastname && formik.errors.lastname && (
+          <p className="text-red-500">{formik.errors.lastname}</p>
+        )}
+      </div>
+    </div>
+
+    <div>
+      <input
+        className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+        type="text"
+        name="email"
+        placeholder="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.email && formik.errors.email && (
+        <p className="text-red-500">{formik.errors.email}</p>
+      )}
+    </div>
+
+    <div>
+      <input
+        className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+        type="text"
+        name="address"
+        placeholder="Address"
+        value={formik.values.address}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.address && formik.errors.address && (
+        <p className="text-red-500">{formik.errors.address}</p>
+      )}
+    </div>
+
+    <div className="flex lg:flex-row flex-col gap-3">
+      <div>
+        <input
+          className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+          type="text"
+          name="city"
+          placeholder="City"
+          value={formik.values.city}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.city && formik.errors.city && (
+          <p className="text-red-500">{formik.errors.city}</p>
+        )}
+      </div>
+
+      <div>
+        <input
+          className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+          type="text"
+          name="state"
+          placeholder="State/Province"
+          value={formik.values.state}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.state && formik.errors.state && (
+          <p className="text-red-500">{formik.errors.state}</p>
+        )}
+      </div>
+
+      <div>
+        <input
+          className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
+          type="text"
+          name="postalCode"
+          placeholder="ZIP/Postal Code"
+          value={formik.values.postalCode}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.postalCode && formik.errors.postalCode && (
+          <p className="text-red-500">{formik.errors.postalCode}</p>
+        )}
+      </div>
+    </div>
+
+    <input
+      className="lg:text-2xl sm:text-xl text-sm font-bold tracking-wider bg-gradient-to-r from-purple-400 to-blue-600 text-transparent text-white py-4 lg:px-10 px-2 rounded tracking-wider lg:mt-6 mt-2 hover:cursor-pointer "
+      type="submit"
+      value="Submit"
+    />
+  </form>
+</div>
+
+const paymentPart = <div>hello</div>
+
+  
+
   return (
-    <div className="font-montserrat items-center flex flex-col justify-center h-screen bg-light1 gap-1">
-      <div className="gap-2 flex lg:flex-row flex-col bg-light3 p-2 rounded-[1em]">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold lg:py-3 py-2 lg:px-[2.5em] px-8 rounded lg:text-xl text-sm tracking-widest">
+    <div className="font-montserrat items-center flex flex-col justify-center bg-light1 gap-1 pt-[8em] pb-[2em] lg:h-screen">
+      <div className="gap-2 flex sm:flex-row flex-col bg-light3 p-2 rounded-[1em]">
+        <button onClick={() => setToggle(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold lg:py-3 py-2 lg:px-[2.5em] px-8 rounded lg:text-xl text-sm tracking-widest">
           Delivery Address
         </button>
-        <button className="hover:bg-blue-500 hover:text-white border-2 font-bold lg:py-3 py-2 lg:px-[2.5em] px-8 rounded lg:text-xl text-sm tracking-widest">
+        <button onClick={() => setToggle(false)} className="hover:bg-blue-500 hover:text-white border-2 font-bold lg:py-3 py-2 lg:px-[2.5em] px-8 rounded lg:text-xl text-sm tracking-widest">
           Payment Method
         </button>
       </div>
-      <div className=" min-w-[80vw] h-auto p-10 bg-white rounded-[1em] shadow-2xl">
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col min-w-[60vw] justify-center w-[30vw] gap-5"
-        >
-          <div className="flex lg:flex-row flex-col justify-between">
-            <div>
-              <input
-                className="border-2 lg:h-[3em] h-[2em] min-w-[30em] placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-                type="text"
-                name="name"
-                placeholder="First name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.name && formik.errors.name && (
-                <p className="text-red-500">{formik.errors.name}</p>
-              )}
-            </div>
-            <div>
-              <input
-                className="border-2 lg:h-[3em] h-[2em] min-w-[30em]  placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-                type="text"
-                name="lastname"
-                placeholder="Last name"
-                value={formik.values.lastname}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.lastname && formik.errors.lastname && (
-                <p className="text-red-500">{formik.errors.lastname}</p>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <input
-              className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.email && formik.errors.email && (
-              <p className="text-red-500">{formik.errors.email}</p>
-            )}
-          </div>
-
-          <div>
-            <input
-              className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-              type="text"
-              name="address"
-              placeholder="Address"
-              value={formik.values.address}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.address && formik.errors.address && (
-              <p className="text-red-500">{formik.errors.address}</p>
-            )}
-          </div>
-
-          <div className="flex lg:flex-row flex-col gap-3">
-            <div>
-              <input
-                className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-                type="text"
-                name="city"
-                placeholder="City"
-                value={formik.values.city}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.city && formik.errors.city && (
-                <p className="text-red-500">{formik.errors.city}</p>
-              )}
-            </div>
-
-            <div>
-              <input
-                className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-                type="text"
-                name="state"
-                placeholder="State/Province"
-                value={formik.values.state}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.state && formik.errors.state && (
-                <p className="text-red-500">{formik.errors.state}</p>
-              )}
-            </div>
-
-            <div>
-              <input
-                className="border-2 lg:h-[3em] h-[2em] w-full placeholder:tracking-wider placeholder:pl-4 placeholder:text-sm"
-                type="text"
-                name="postalCode"
-                placeholder="ZIP/Postal Code"
-                value={formik.values.postalCode}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {formik.touched.postalCode && formik.errors.postalCode && (
-                <p className="text-red-500">{formik.errors.postalCode}</p>
-              )}
-            </div>
-          </div>
-
-          <input
-            className="lg:text-2xl sm:text-xl text-sm font-bold tracking-wider bg-gradient-to-r from-purple-400 to-blue-600 text-transparent text-white py-4 lg:px-10 px-2 rounded tracking-wider lg:mt-6 mt-2 hover:cursor-pointer "
-            type="submit"
-            value="Submit"
-          />
-        </form>
-      </div>
+     {isToggle ? deliveryPart : paymentPart}
     </div>
   );
 }
