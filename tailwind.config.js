5c79231b24b43301106e5ce155caed94fad6972f
SeymaDemir <seymademir35@gmail.com>
2023-06-09 08:32:52 +0300

    fixed thank you page bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+     
       fontFamily: {
-        montserrat:["Montserrat"],
-        lilita:["Lilita One"],
-        bakbak:["Bakbak One"]
+        montserrat: ["Montserrat"],
+        lilita: ["Lilita One"],
+        bakbak: ["Bakbak One"],
       },
-      colors:{
-        dark1:"#272B34",
-        dark2:"#6E7487",
-        dark3:"#B2B5BA",
-        light1:"#F3F2F7",
-        light2:"#EDEBF1",
-        light3:"#DFE4FA",
-        error:"#EE5142",
-        info:"#46C275"
+      colors: {
+        dark1: "#272B34",
+        dark2: "#6E7487",
+        dark3: "#B2B5BA",
+        light1: "#F3F2F7",
+        light2: "#EDEBF1",
+        light3: "#DFE4FA",
+        error: "#EE5142",
+        info: "#46C275",
       },
-      backgroundImage:{
-        landing:"url('/home/seymademir/Development/code/React/hoomieproject/src/images/landingPageBg.jpg')"
-      }
+      backgroundImage: {
+        landing:
+          "url('/home/seymademir/Development/code/React/hoomieproject/src/images/landingPageBg.jpg')",
+          "pinkbg":'url("/home/seymademir/Development/code/React/hoomieproject/src/images/pinkthanks.jpeg")',
+      },
     },
   },
   plugins: [],
-}
+};
