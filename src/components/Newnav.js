bd64c035255f144bf2722ceaf934feee188379c8
SeymaDemir <seymademir35@gmail.com>
2023-07-04 15:42:41 +0300

    .

--- src/components/Newnav.js
+++ src/components/Newnav.js
@@ -64,7 +64,7 @@ function Newnav() {
         </div>
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 opacity-100" : "top-[-490px]"
+            open ? "top-24 opacity-100" : "top-[-490px]"
           } md:opacity-100 opacity-0`}
         >
           {Links.map((link) => (
